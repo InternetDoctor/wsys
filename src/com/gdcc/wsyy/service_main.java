0c7b931bb3b1d126a87a958a3c4f36b84633bab0
redcat001 <13728057231@163.com>
2015-10-08 18:44:12 +0800

    146

---
+++ src/com/gdcc/wsyy/service_main.java
@@ -0,0 +1,24 @@
+package com.gdcc.wsyy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class service_main extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View view=inflater.inflate(R.layout.service_m_main, null);
+		
+		
+		return view;
+	}
+	
+	
+	
+
+}
